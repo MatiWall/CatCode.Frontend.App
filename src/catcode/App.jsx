f6253bbf458c2ebc29199de34ac38660b244a1ff
Matias Wallenius <MatiWall@live.dk>
2025-01-08 21:25:41 +0100

    fixed config, did not match schema

--- src/catcode/App.jsx
+++ src/catcode/App.jsx
@@ -1,11 +1,37 @@
 import { createApp } from "@plugger/app";
-import { rootExtensionBluePrint } from "@plugger/extension";
+import { rootExtensionBluePrint, createExtension , rootComponentRef} from "@plugger/extension";
+import { EnvConfigLoader } from '@plugger/configuration-loader'
+
+import {z} from 'zod';
 
 
+const testExtension = createExtension({
+    namespace: 'app',
+    name: 'app',
+    kind: 'component',
+    attachToo: {namespace: 'root', name: 'app', kind: 'component'},
+    output: [
+        rootComponentRef
+    ], 
+    configSchema: z.object({
+        title: z.string().default('hello from schema')
+    }),
+    provider: ({input, config}) => {
+        return [
+            rootComponentRef.with(<div>this is from my custom extension {config.title}</div>)
+        ]
+    }
+})
 
 const app = createApp({
-    rootExtensions: [rootExtensionBluePrint.make()],
+    rootExtensions: [
+        rootExtensionBluePrint.make(),
+        testExtension
+    ],
+    configLoader: new EnvConfigLoader('APP_CONFIG')
 });
+
+console.log(app.configLoader.getExtensionConfig('app', 'app', 'component'))
 
 const Root = app.createRoot();
 export default Root
